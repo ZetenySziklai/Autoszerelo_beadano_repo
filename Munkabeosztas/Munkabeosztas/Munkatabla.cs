0badee07c339352b7bbf02fff664ff9a41ccb892
Sziklai Zétény <sziklai.zeteny@ckik.loc>
2025-02-21 12:25:09 +0100

    proba

--- Munkabeosztas/Munkabeosztas/Munkatabla.cs
+++ Munkabeosztas/Munkabeosztas/Munkatabla.cs
@@ -47,15 +47,22 @@ namespace Munkabeosztas
         private static List<int> munkakor()
         {
             List<int> orak = new List<int>();
+            List<Munkabeosztas> munkalek = munkalekeres();
             for (int i = 0; i < lista.Count; i++)
             {
-                for (int j = 0;  j < lista[i].munka_ido.Count; j++)
+                for ()
                 {
+                    for (int j = 0;  j < lista[i].munka_ido.Count; j++)
+                
+                    {
                     if (lista[i].munka_ido[j] == true)
                     {
                         orak.Add(i+1);
                     }
                 }
+
+                }
+                
             }
             return orak;
 
@@ -67,12 +74,51 @@ namespace Munkabeosztas
             return rendezes;
         }
 
-        private static List<Munkabeosztas> foglalas()
+        private static void foglalas()
         {
             int ora_kezdete = Convert.ToInt32(Console.ReadLine());
             int ora_vege = Convert.ToInt32(Console.ReadLine());
+            bool igazh = true;
             List<Munkabeosztas> munkasok = new List<Munkabeosztas>();
             munkasok = munkalekeres();
+            for (int i = 0; i < munkasok.Count; i++)
+            {
+                for (int j = 0; j < lista[i].munka_ido.Count; j++)
+                {
+                    igazh = eldontes(ora_kezdete, ora_vege, i, j);
+
+                }
+            }
+
+            if (igazh == true)
+            {
+                Console.WriteLine("van szabadhely");
+            }
+            else
+            {
+                Console.WriteLine("nincs szabadhely");
+            }
+
+        }
+        private static bool eldontes(int ora_kezdete, int ora_vege,int i,int j)
+        {
+            for (int k = ora_kezdete; k <= ora_vege; k++)
+            {
+                if (lista[i].munka_ido[j] == false)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+            return true;
+        }
+
+        private static List<int> osszesora()
+        {
 
         }
     }
